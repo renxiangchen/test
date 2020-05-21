f309ed8a106f266234c613db9c76693a6e4e0fdc
renxiangchen <renxiang.chen@xcalibyte.com>
2020-05-21 13:56:59 +0800

    agent test

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,15 @@ FROM python:3.8
 
 COPY entrypoint.sh /entrypoint.sh
 
+RUN cd /
+RUN mkdir xc
+
+WORDKIR /xc
+
+ADD PoC1-0-10-c.tar .
+
+RUN ls
+
 ENTRYPOINT ["/entrypoint.sh"]
+
+
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,6 @@
 cd /
 mkdir xc 
 cd xc
-WORKDIR /xc
-ADD PoC1-0-10-c.tar . 
-ls
-echo "aafdaae:wq"
-echo "Hello $1"
+echo Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
