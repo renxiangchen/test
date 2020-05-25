6b5f219ff35c596d1d7ac0f522b3379d2ebe77f6
renxiangchen <renxiang.chen@xcalibyte.com>
2020-05-25 15:23:47 +0800

    entrypoint.sh 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -l 
 
-echo "Hello $1"
+cd /xcalagent/tools
+ls -al
+ping mac.xcalibyte.com -c 1
+#python3 /xcalagent/tools/xcal-scanner.py -d -sc /xcalagent/workdir/run.conf -pc /xcalagent/tools/xcal-project.conf -usc -np
+
 time=$(date)
 echo "::set-output name=time::$time"
