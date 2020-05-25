d7f2307abce75fd48f41335aae6c356c5c797234
renxiangchen <renxiang.chen@xcalibyte.com>
2020-05-25 19:09:23 +0800

    xcalbuild

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ ping 39.108.212.149 -c 1
 python3 xcal-scanner.py -d -sc ../workdir/run.conf -pc xcal-project.conf -usc 
 #python3 /xcalagent/tools/xcal-scanner.py -d -sc /xcalagent/workdir/run.conf -pc /xcalagent/tools/xcal-project.conf -usc 
 ls /xcalagent/workdir/jobs
+cat /xcalagent/workdir/jobs/*/xcalbuild.log
 
 
 time=$(date)
