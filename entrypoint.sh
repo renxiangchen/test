bdfaffb0f6f8d671f2afbae8928c2f7ba8248a4e
renxiangchen <renxiang.chen@xcalibyte.com>
2020-05-29 12:08:44 +0800

    testa

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l 
 cd /xcalagent/tools
 ls -al
-python3 xcal-scanner.py -d -sc ../workdir/run.conf -pc xcal-project.conf --build-path /goaccess-1.3 --project-path /goaccess-1.3 -usc 
+python3 xcal-scanner.py -d -sc ../workdir/run.conf -pc xcal-project.conf --build-path /goaccess-1.3 --project-path /goaccess-1.3 -usc -np
 #python3 /xcalagent/tools/xcal-scanner.py -d -sc /xcalagent/workdir/run.conf -pc /xcalagent/tools/xcal-project.conf -usc 
 
 time=$(date)
